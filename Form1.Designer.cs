7de9168b126eeb5f21fedb5374e88a1688456473
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-24 11:26:03 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Crud_CC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            button1 = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(192, 0, 0);
+            label1.Location = new Point(387, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(408, 38);
+            label1.TabIndex = 0;
+            label1.Text = "FORMULAIRE D'INSCRIPTION";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(280, 134);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 28);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(52, 134);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Nom Projet\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(52, 189);
+            label3.Name = "label3";
+            label3.Size = new Size(152, 30);
+            label3.TabIndex = 4;
+            label3.Text = "Theme Projet\r\n";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(280, 188);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(300, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(52, 244);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 30);
+            label4.TabIndex = 6;
+            label4.Text = "Date Debut";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(166, 407);
+            button1.Name = "button1";
+            button1.Size = new Size(237, 61);
+            button1.TabIndex = 7;
+            button1.Text = "Creer\r\n";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(280, 244);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 31);
+            dateTimePicker1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(0, 192, 0);
+            button2.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(462, 407);
+            button2.Name = "button2";
+            button2.Size = new Size(225, 61);
+            button2.TabIndex = 9;
+            button2.Text = "Voir";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = Color.FromArgb(224, 224, 224);
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(642, 100);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(423, 212);
+            dataGridView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 192, 255);
+            ClientSize = new Size(1077, 511);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private Button button2;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+using System.Data;
+using MySqlConnector;
+
+namespace Crud_CC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectString = "Server=localhost;User ID=root;Password='';Database=gestion_projet_cc;AllowZeroDatetime=true;";
+            try
+            {
+                using (MySqlConnection connect = new MySqlConnection(connectString))
+                {
+                    connect.Open();
+
+                    // Préparez la commande SQL pour l'insertion
+                    MySqlCommand command = new MySqlCommand("INSERT INTO projets (appellation_projet, theme_projet, date_debut_projet) VALUES (@appellation_projet, @theme_projet, @date_debut_projet)", connect);
+
+                    // Ajoutez les paramètres à la commande
+                    //command.Parameters.AddWithValue("@id_projet", int.Parse(textBox1.Text));
+                    command.Parameters.AddWithValue("@appellation_projet", textBox1.Text);
+                    command.Parameters.AddWithValue("@theme_projet", textBox2.Text);
+                    command.Parameters.AddWithValue("@date_debut_projet", dateTimePicker1.Value);
+
+                    // Exécutez la commande
+                    command.ExecuteNonQuery();
+
+                    // Optionnel : Afficher un message de succès
+                    MessageBox.Show("projet ajouté avec succès.");
+
+                    //LoadData();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Gérer les erreurs
+                MessageBox.Show("Une erreur est survenue : " + ex.Message);
+            }
+        }
+
+        //private void LoadData()
+        //{
+        //    string connectString = "Server=localhost;User ID=root;Password='';Database=gestion_projet_cc;AllowZeroDatetime=true;";
+        //    try
+        //    {
+        //        using (MySqlConnection connect = new MySqlConnection(connectString))
+        //        {
+        //            connect.Open();
+
+        //            // Préparez la commande SQL pour récupérer les données
+        //            MySqlCommand command = new MySqlCommand("SELECT * FROM projets", connect);
+        //            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+        //            DataTable dataTable = new DataTable();
+
+        //            // Remplissez le DataTable
+        //            adapter.Fill(dataTable);
+
+        //            // Assignez le DataTable au DataGridView
+        //            dataGridView1.DataSource = dataTable;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Gérer les erreurs
+        //        MessageBox.Show("Erreur lors du chargement des données : " + ex.Message);
+        //    }
+        //}
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+                textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            }
+
+        }
+    }
+}
